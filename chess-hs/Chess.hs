321272b25db46028c9a4b51313260465838a9077
Benjamin Cape <benjcape@gmail.com>
2021-07-30 09:30:41 -0400

    cleanup

--- chess-hs/Chess.hs
+++ chess-hs/Chess.hs
@@ -5,31 +5,25 @@ import           Data.Char
 import           Data.List               hiding ( lookup )
 import           Data.Map                       ( Map
                                                 , assocs
+                                                , delete
                                                 , elems
                                                 , filter
                                                 , fromList
                                                 , insert
                                                 , lookup
                                                 , mapMaybe
-                                                , update
                                                 )
 import           Data.Maybe
-import           Data.Set                       ( Set
-                                                , empty
-                                                , fromAscList
-                                                , toList
-                                                , union
-                                                )
 import           Data.Text
 import           Prelude                 hiding ( head
                                                 , lookup
                                                 )
 
--- Parsing a Structure from a String
+-- Type Class for parsing a Structure from a String
 class Parse n where
   parse :: String -> n
 
--- Display a Structure in a human readable format
+-- Type Class for displaying a Structure in a human readable format
 class Display n where
   display :: n -> String
 
@@ -121,9 +115,7 @@ type Board = Map Position Piece
 -- Get the current position, if any of a piece
 piecePosition :: GameState -> Piece -> Maybe Position
 piecePosition game piece =
-  case Data.List.find (\(_, v) -> v == piece) (assocs (board game)) of
-    Nothing  -> Nothing
-    Just pos -> Just (fst pos)
+  (Just . fst) =<< Data.List.find (\(_, v) -> v == piece) (assocs (board game))
 
 -- Parsing a Board helper
 parseSplitBoard :: [Text] -> Board
@@ -148,6 +140,19 @@ parseSplitBoard text = Data.Map.mapMaybe
 parseBoard :: Text -> Board
 parseBoard str = parseSplitBoard (chunksOf 1 str)
 
+
+checkUniqueHelper :: Eq a => [a] -> [a] -> Bool
+checkUniqueHelper _ [] = True
+checkUniqueHelper seen (next : remain) =
+  notElem next seen && checkUniqueHelper (next : seen) remain
+
+checkUnique :: Eq a => [a] -> Bool
+checkUnique = checkUniqueHelper []
+
+-- Is a board valid?
+validBoard :: Board -> Bool
+validBoard b = checkUnique (elems b)
+
 -- Data Structure for a Game, holds the current turn, and a board
 data GameState = Game
   { turn  :: Color
@@ -167,18 +172,14 @@ currentTurnPieces game =
 -- Determine's which color occupies a given position on the board, if at all
 -- This is never used though...
 occupantColor :: Board -> Position -> Maybe Color
-occupantColor b pos = case lookup pos b of
-  Nothing -> Nothing
-  Just p  -> Just (color p)
+occupantColor b pos = (Just . color) =<< lookup pos b
 
 -- Moves the piece in position 1 to position 2
-move :: GameState -> Position -> Position -> GameState
-move game start end = case lookup start (board game) of
+move :: GameState -> Move -> GameState
+move game (start, end) = case lookup start (board game) of
   Nothing -> game
   Just p  -> do
-    let newBoard = Data.Map.update (const Nothing)
-                                   start
-                                   (Data.Map.insert end p (board game))
+    let newBoard = Data.Map.delete start (Data.Map.insert end p (board game))
     Game { board = newBoard, turn = nextColor (turn game) }
 
 instance Display GameState where
@@ -209,20 +210,22 @@ instance Display GameState where
 
 -- Parse helper to parse each individual Char (Text) 
 parseSplitString :: [Text] -> GameState
-parseSplitString text = Game { turn  = parse (unpack (Data.List.head text))
-                             , board = parseBoard (text !! 1)
-                             }
+parseSplitString text = do
+  let parsed = parseBoard (text !! 1)
+  if not (validBoard parsed)
+    then error "Invalid Board"
+    else Game { turn = parse (unpack (Data.List.head text)), board = parsed }
 
 instance Parse GameState where
   parse str = parseSplitString (splitOn (pack ": ") (pack str))
 
--- Returns true if the current player can move ANY piece to the provided position
+-- Returns true if the current position is not occupied by the current player 
 filterMove :: GameState -> Position -> Bool
 filterMove game pos = case lookup pos (board game) of
   Nothing -> True
   Just p  -> color p /= turn game
 
--- Returns a list of valid positions for ANY piece, i.e. each output location is a valid place the SOME piece can move to
+-- Given a list of potential pieces, return a subset of them if they are pieces and they are not occupied by the current player
 filterMoves :: GameState -> [Maybe Position] -> [Position]
 filterMoves game positions =
   Prelude.filter (filterMove game) (catMaybes positions)
@@ -230,10 +233,10 @@ filterMoves game positions =
 -- Helper to recursively collect positions that a rook can move to
 rookMovesHelper
   :: (Position -> Maybe Position) -> GameState -> Position -> [Position]
-rookMovesHelper f game current = do
-  let next = f current
+rookMovesHelper mover game current = do
+  let next = mover current
   case (\x -> lookup x (board game)) =<< next of
-    Nothing -> maybe [] (\x -> x : rookMovesHelper f game x) next
+    Nothing -> maybe [] (\x -> x : rookMovesHelper mover game x) next
     Just p  -> maybe [] (\x -> [ x | turn game /= color p ]) next
 
 -- Get all moves a rook can move to
@@ -249,6 +252,8 @@ rookMoves game pos =
 -- No piece can move to a location occupied by a piece of the same color
 -- Captures - SAME
 
+-- Calculates the potential moves from a given square to another square, first by finding what piece is on that square, 
+-- and then figuring out where it can move to
 potentialSquaresPos :: GameState -> Position -> [Position]
 potentialSquaresPos game start = case lookup start (board game) of
   Nothing -> []
@@ -259,17 +264,35 @@ potentialSquaresPos game start = case lookup start (board game) of
       [prevPosition =<< prevPosition start, nextPosition =<< nextPosition start]
     Rook -> rookMoves game start
 
-potentialMovePos :: GameState -> Position -> [(Position, Position)]
-potentialMovePos game start =
-  Data.List.map (start, ) (potentialSquaresPos game start)
-
+-- This is the same as potentialSquaresPos, but instead of passing in a position, we pass in a piece, and get it's position to perform the calculation
 potentialSquares :: GameState -> Piece -> [Position]
 potentialSquares game piece =
   maybe [] (potentialSquaresPos game) (piecePosition game piece)
 
+-- This tells us, given a state, where can the CURRENT player move to. 
 potentialSquaresTurn :: GameState -> [Position]
 potentialSquaresTurn game =
   Data.List.concatMap (potentialSquares game) (currentTurnPieces game)
+
+-- The following three functions are meant to be counterparts to the above three, just returning moves rather than destinations
+
+type Move = (Position, Position)
+
+-- Given a position, what are the potential moves, as tuples. The first element in the tuple will always be 
+-- the second parameter to this function
+potentialMovePos :: GameState -> Position -> [Move]
+potentialMovePos game start =
+  Data.List.map (start, ) (potentialSquaresPos game start)
+
+  -- Get all potential moves for a specific piece
+potentialMoves :: GameState -> Piece -> [Move]
+potentialMoves game piece =
+  maybe [] (potentialMovePos game) (piecePosition game piece)
+
+-- Get all potential moves for the current turn
+potentialMovesTurn :: GameState -> [Move]
+potentialMovesTurn game =
+  Data.List.concatMap (potentialMoves game) (currentTurnPieces game)
 
 -- Checks if the other persons King can be captured currently
 kingInCheck :: GameState -> Bool
@@ -280,25 +303,14 @@ kingInCheck game = do
     Nothing -> False
     Just p  -> p `elem` potentialSquaresTurn game
 
--- Get all potential moves for a specific piece
-potentialMoves :: GameState -> Piece -> [(Position, Position)]
-potentialMoves game piece =
-  maybe [] (potentialMovePos game) (piecePosition game piece)
-
--- Get all potential moves for the current turn
-potentialMovesTurn :: GameState -> [(Position, Position)]
-potentialMovesTurn game =
-  Data.List.concatMap (potentialMoves game) (currentTurnPieces game)
-
 -- A move is legal if the king of the person who just went is not in check
 isLegal :: GameState -> Bool
 isLegal game = not (kingInCheck game)
 
 -- Collect all legal moves from a given state
 legalMoves :: GameState -> [GameState]
-legalMoves game = Data.List.filter
-  isLegal
-  (Data.List.map (uncurry (move game)) (potentialMovesTurn game))
+legalMoves game =
+  Data.List.filter isLegal (Data.List.map (move game) (potentialMovesTurn game))
 
 -- Constant Initial Position
 initialPosition :: GameState
@@ -317,5 +329,5 @@ countStatesHelper previous (game : games) = do
   (c + 1, res)
 
 -- Count all the possible states that are reachable from a provided state, and return all the states
-countStates :: GameState -> (Int, [GameState])
-countStates game = countStatesHelper [] [game]
+countStates :: GameState -> Int
+countStates game = fst (countStatesHelper [] [game])
